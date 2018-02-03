d5d2f752c528e12de67e1376b9dbea45b42b3e1f
Matthew Hodgkins <matthodge@gmail.com>
2018-02-03 22:46:25 +0100

    moar fixes

--- Windows2016StdCore-TCBuildAgent/install_tc_build_agent.ps1
+++ Windows2016StdCore-TCBuildAgent/install_tc_build_agent.ps1
@@ -2,11 +2,13 @@ $ErrorActionPreference = 'Stop'
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install teamcityagent -y
+
 choco install git -y
 choco install packer -y
 choco install virtualbox -y
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\usr\bin", "Machine")
+choco install server-jre8
+choco install teamcityagent -y -params "serverurl=$($env:TEAMCITY_HOST_URL)"
 
 $tcAgentConfig = @"
 serverUrl=$($env:TEAMCITY_HOST_URL)
