99342da2ce7bad243f518b04fbfb2101cc1bf62d
Matthew Hodgkins <matthodge@gmail.com>
2018-02-03 22:17:54 +0100

    Update TCBuild

--- Windows2016StdCore-TCBuildAgent/install_tc_build_agent.ps1
+++ Windows2016StdCore-TCBuildAgent/install_tc_build_agent.ps1
@@ -1,8 +1,22 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-Set-ExecutionPolicy RemoteSigned -Force
+$ErrorActionPreference = 'Stop'
 
-choco install teamcityagent -params "serverurl=$($env:TEAMCITY_HOST_URL)" -y
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install teamcityagent -y
 choco install git -y
 choco install packer -y
 choco install virtualbox -y
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\usr\bin", "Machine")
+
+$tcAgentConfig = "
+serverUrl=$($env:TEAMCITY_HOST_URL)
+name=VirtualBox
+workDir=../work
+tempDir=../temp
+systemDir=../system
+teamcity.agent.provides.hyperv true"
+
+Set-Content -Path C:\buildAgent\conf\buildAgent.properties -Value $tcAgentConfig -Force
+
+Restart-Service -Name TCBuildAgent
+Start-Service -Name TCBuildAgent
